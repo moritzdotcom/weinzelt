cea14bce9064adfaa8dad4fffbcb221eb9760215
Moritz Löchner <momo.loechner@gmail.com>
2025-05-06 14:07:33 +0200

    New Meta Tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,42 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Weinzelt</title>
+        <meta
+          name="description"
+          content="Das Weinzelt auf der Düsseldorfer Rheinkirmes - Wine meets Rheinkirmes"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:image"
+          content={encodeURI('https://weinzelt.vercel.app/home/weinzelt.png')}
+        />
+        <meta name="twitter:url" content="https://dasweinzelt.de" />
+        <meta name="twitter:title" content="Weinzelt" />
+        <meta
+          name="twitter:description"
+          content="Das Weinzelt auf der Düsseldorfer Rheinkirmes - Wine meets Rheinkirmes"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content={encodeURI('https://weinzelt.vercel.app/home/weinzelt.png')}
+        />
+        <meta property="og:title" content="Weinzelt" />
+        <meta
+          property="og:description"
+          content="Das Weinzelt auf der Düsseldorfer Rheinkirmes - Wine meets Rheinkirmes"
+        />
+        <meta property="og:site_name" content="Weinzelt" />
+        <meta property="og:url" content="https://dasweinzelt.de" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
