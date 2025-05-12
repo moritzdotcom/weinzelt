9b109f1732479f3bbc3db2a10b9ebfebdba3ea4b
Moritz Löchner <momo.loechner@gmail.com>
2025-05-12 22:38:49 +0200

    Add createdAt to reservation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import { Divider, Drawer } from '@mui/material';
+import { Divider } from '@mui/material';
 import CountdownSection from '@/components/countdown';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
 import { Session } from '@/hooks/useSession';
 import { KeyboardArrowRight } from '@mui/icons-material';
 import { events } from '@/lib/events';
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
 
 export default function Home({ session }: { session: Session }) {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <div className="font-sans relative">
       <div className="fixed bottom-0 left-0 w-full sm:hidden z-10 bg-gray-100 px-3 py-2 flex items-center justify-center">
@@ -25,98 +20,7 @@ export default function Home({ session }: { session: Session }) {
           <KeyboardArrowRight fontSize="inherit" />
         </Link>
       </div>
-      <header className="fixed w-full bg-white shadow z-50">
-        {session.status == 'authenticated' && (
-          <div className="w-full bg-gray-100">
-            <div className="max-w-7xl mx-auto px-4 py-1 flex items-center justify-between">
-              <p className="font-bold">Hallo {session.user.name}!</p>
-              <Link
-                href="/backend"
-                className="flex items-center gap-1 underline"
-              >
-                Zur Admin Seite
-              </Link>
-            </div>
-          </div>
-        )}
-        <div className="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
-          <Link href="#">
-            <img src="/logo-sm.png" alt="WEINZELT" className="w-32 sm:w-40" />
-          </Link>
-          <nav className="hidden md:flex gap-6 items-center">
-            <Link href="#vip">VIP</Link>
-            <Link href="#musik">Musik</Link>
-            <Link href="#gastro">Speisen & Getränke</Link>
-            <Link href="#partner">Partner</Link>
-            <Link
-              href="/reservation"
-              className="inline-block bg-black text-white px-3 py-2 rounded-full shadow hover:bg-gray-300 hover:text-black"
-            >
-              Tisch reservieren
-            </Link>
-          </nav>
-          <button className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
-            {menuOpen ? <CloseIcon /> : <MenuIcon />}
-          </button>
-        </div>
-        <Drawer
-          className="md:hidden"
-          anchor="right"
-          open={menuOpen}
-          onClose={() => setMenuOpen(false)}
-        >
-          <div className="p-5 flex flex-col justify-between text-left text-xl min-w-[70vw] h-full">
-            <div className="flex flex-col gap-4">
-              <div className="flex justify-end">
-                <button onClick={() => setMenuOpen(false)}>
-                  <CloseIcon fontSize="large" />
-                </button>
-              </div>
-              <Link href="#" onClick={() => setMenuOpen(false)}>
-                Start
-              </Link>
-              <Divider />
-              <Link href="#vip" onClick={() => setMenuOpen(false)}>
-                VIP-Tische
-              </Link>
-              <Divider />
-              <Link href="#standing-tables" onClick={() => setMenuOpen(false)}>
-                Stehtische
-              </Link>
-              <Divider />
-              <Link href="#musik" onClick={() => setMenuOpen(false)}>
-                Musik
-              </Link>
-              <Divider />
-              <Link href="#gastro" onClick={() => setMenuOpen(false)}>
-                Speisen & Getränke
-              </Link>
-              <Divider />
-              <Link href="#partner" onClick={() => setMenuOpen(false)}>
-                Partner
-              </Link>
-              <Divider />
-              <Link href="/reservation">Tisch reservieren</Link>
-            </div>
-            <div className="mx-auto flex gap-5 items-center text-4xl">
-              <Link
-                href="https://www.instagram.com/weinzelt.dus/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <InstagramIcon fontSize="inherit" />
-              </Link>
-              <Link
-                href="https://www.facebook.com/weinzelt.dus/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FacebookIcon fontSize="inherit" />
-              </Link>
-            </div>
-          </div>
-        </Drawer>
-      </header>
+      <Navbar session={session} />
 
       {/* Hero */}
       <section className="relative h-[50vh] sm:h-[80vh] flex items-center justify-center bg-black text-white">
@@ -135,7 +39,7 @@ export default function Home({ session }: { session: Session }) {
           Weinzelt - Wine meets Rheinkirmes
         </h2>
         <h3 className="text-2xl italic font-medium text-center text-gray-700 mb-10">
-          „Von Düsseldorfern für alle die Lust auf Wein haben!“
+          „Von Düsseldorfern für alle, die Lust auf Wein haben!“
         </h3>
         <div className="grid md:grid-cols-[7fr_4fr] gap-10 items-center">
           <div className="space-y-4">
@@ -439,38 +343,7 @@ export default function Home({ session }: { session: Session }) {
         </div>
       </section>
 
-      {/* Footer */}
-      <footer className="bg-black text-white text-sm pt-6 pb-16 sm:pb-8 px-4">
-        <div className="w-full max-w-6xl mx-auto flex gap-5 flex-col sm:flex-row items-center sm:justify-between">
-          <div>
-            <p>&copy; 2025 Weinzelt GmbH</p>
-            <div className="mt-2">
-              <Link href="/imprint" className="underline mr-2">
-                Impressum
-              </Link>
-              <Link href="/privacy" className="underline">
-                Datenschutz
-              </Link>
-            </div>
-          </div>
-          <div className="flex gap-5 items-center text-3xl">
-            <Link
-              href="https://www.instagram.com/weinzelt.dus/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <InstagramIcon fontSize="inherit" />
-            </Link>
-            <Link
-              href="https://www.facebook.com/weinzelt.dus/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FacebookIcon fontSize="inherit" />
-            </Link>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
