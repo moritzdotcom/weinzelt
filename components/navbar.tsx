a59dfa0b44b17b74ccc9b54d23b4ec4eaf25dd83
Moritz Löchner <momo.loechner@gmail.com>
2025-05-15 11:50:23 +0200

    Start Dropdown

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,17 @@
 import { Session } from '@/hooks/useSession';
-import { Drawer, Divider } from '@mui/material';
+import { Drawer, Divider, Collapse, IconButton } from '@mui/material';
 import Link from 'next/link';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import { useLayoutEffect, useRef, useState } from 'react';
 
 export default function Navbar({ session }: { session: Session }) {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [startOpen, setStartOpen] = useState(false);
   const headerRef = useRef<HTMLElement>(null);
   const [headerHeight, setHeaderHeight] = useState(0);
 
@@ -75,34 +78,51 @@ export default function Navbar({ session }: { session: Session }) {
           <div className="p-5 flex flex-col justify-between text-left text-xl min-w-[70vw] h-full">
             <div className="flex flex-col gap-4">
               <div className="flex justify-end">
-                <button onClick={() => setMenuOpen(false)}>
+                <IconButton onClick={() => setMenuOpen(false)}>
                   <CloseIcon fontSize="large" />
-                </button>
+                </IconButton>
               </div>
-              <Link href="/#" onClick={() => setMenuOpen(false)}>
-                Start
-              </Link>
+
+              {/* START Dropdown */}
+              <div>
+                <button
+                  className="w-full flex items-center justify-between"
+                  onClick={() => setStartOpen((prev) => !prev)}
+                >
+                  <span className="text-left">Start</span>
+                  {startOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                </button>
+                <Collapse in={startOpen} timeout="auto" unmountOnExit>
+                  <div className="flex flex-col gap-3 mt-3 ml-3 text-neutral-500">
+                    <Link href="/#vip" onClick={() => setMenuOpen(false)}>
+                      VIP-Tische
+                    </Link>
+                    <Divider />
+                    <Link
+                      href="/#standing-tables"
+                      onClick={() => setMenuOpen(false)}
+                    >
+                      Stehtische
+                    </Link>
+                    <Divider />
+                    <Link href="/#musik" onClick={() => setMenuOpen(false)}>
+                      Musik & Events
+                    </Link>
+                    <Divider />
+                    <Link href="/#gastro" onClick={() => setMenuOpen(false)}>
+                      Speisen & Getränke
+                    </Link>
+                    <Divider />
+                    <Link href="/#partner" onClick={() => setMenuOpen(false)}>
+                      Partner
+                    </Link>
+                  </div>
+                </Collapse>
+              </div>
+
               <Divider />
-              <Link href="/#vip" onClick={() => setMenuOpen(false)}>
-                VIP-Tische
-              </Link>
-              <Divider />
-              <Link href="/#standing-tables" onClick={() => setMenuOpen(false)}>
-                Stehtische
-              </Link>
-              <Divider />
-              <Link href="/#musik" onClick={() => setMenuOpen(false)}>
-                Musik
-              </Link>
-              <Divider />
-              <Link href="/#gastro" onClick={() => setMenuOpen(false)}>
-                Speisen & Getränke
-              </Link>
-              <Divider />
-              <Link href="/#partner" onClick={() => setMenuOpen(false)}>
-                Partner
-              </Link>
-              <Divider />
+
+              {/* Die restlichen Menüpunkte */}
               <Link href="/about" onClick={() => setMenuOpen(false)}>
                 Über uns
               </Link>
@@ -111,8 +131,12 @@ export default function Navbar({ session }: { session: Session }) {
                 Jobs
               </Link>
               <Divider />
-              <Link href="/reservation">Tisch reservieren</Link>
+              <Link href="/reservation" onClick={() => setMenuOpen(false)}>
+                Tisch reservieren
+              </Link>
             </div>
+
+            {/* Social Icons */}
             <div className="mx-auto flex gap-5 items-center text-4xl">
               <Link
                 href="https://www.instagram.com/weinzelt.dus/"
