0b72f669ffbbb374c82d83925c947f2a45826fbf
Moritz Löchner <momo.loechner@gmail.com>
2025-05-16 08:01:22 +0200

    Make header clickable

--- components/reservation/header.tsx
+++ components/reservation/header.tsx
@@ -1,9 +1,16 @@
 import { Box, Typography } from '@mui/material';
+import Link from 'next/link';
 
 export default function ReservationHeader({ children }: { children: string }) {
   return (
     <Box className="text-center mb-6">
-      <img src="/logo.png" alt="Weinzelt Logo" className="mx-auto h-20 mb-12" />
+      <Link href="/">
+        <img
+          src="/logo.png"
+          alt="Weinzelt Logo"
+          className="mx-auto h-20 mb-12"
+        />
+      </Link>
       <Typography variant="h4" gutterBottom>
         {children}
       </Typography>
